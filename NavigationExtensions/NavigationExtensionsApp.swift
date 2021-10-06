dcf47801a7ef16309658c96227206573e4f623f3
Alexander <cavegn.a@gmx.ch>
2021-10-06 09:34:03 +0200

    Initial Commit

---
+++ NavigationExtensions/NavigationExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExtensionsApp.swift
+//  NavigationExtensions
+//
+//  Created by Alexander on 06.10.21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
